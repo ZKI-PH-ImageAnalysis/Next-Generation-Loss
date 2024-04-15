1eb1b4a9f145a3554e9044caf3d4c3f1ea3489aa
AkhmedovaShakhnaz <107623498+AkhmedovaShakhnaz@users.noreply.github.com>
2024-04-15 11:32:21 +0200

    Update NGL_torch.py
    changed the loss name

--- NGL_torch.py
+++ NGL_torch.py
@@ -1,10 +1,10 @@
 import torch
 from torch import nn
 
-class F21(nn.Module):
+class NGL(nn.Module):
 
     def __init__(self):
-        super(F21, self).__init__()
+        super(NGL, self).__init__()
 
     def forward(self, x, target):
         target = torch.nn.functional.one_hot(target, num_classes=x.size(1))
